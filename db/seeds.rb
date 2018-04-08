151a187cca9ebd5391fc3283a7bf9f3e4715ad74
Tamara Jost <facebookoftjo@gmail.com>
2018-04-08 11:08:08 +0100

    eva anpassungen

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,10 @@ puts 'Previous users destroyed'
 
 users = []
 organisations = ['UBS', 'Credit Suisse']
-departments = ['WTID 12', 'WTRF 62', 'WAAB', 'WTRA 432', 'WAOD 5', 'WTIC 23']
-roles = ['Normal Clerk', 'Normal Clerk', 'Director', 'Super-Chief']
+departments = ['Cross Border Information Management', 'CRS and AEI implementation','IWM Solution Architecture','Us 871m external oversight','Market HEad Middle East and Africa','FRM IT CH', 'US 871m external', 'Solution Architecture', 'DevOps Zurch', 'Product Buffet', 'Performance CH', 'Application Maintenance', 'CM ME', 'Sales Management','Investran']
+roles = ['CEO', 'AVP', 'MxB', 'VP', 'Staff', 'DIR', 'ENO', 'MDR']
 genders = ['Female', 'Male']
-locations = ['Zurich', 'Basel', 'Geneva', 'Lausanne']
-
+locations = ['Uetlibergstrasse', 'Claridenstrasse', 'Kalendergasse', 'Bleicherweg', 'Talacker', 'Europaallee']
 
 
 User.create!(
@@ -21,13 +20,13 @@ User.create!(
   email: 'eva@duju.ch',
   password: 'dujurox',
   organisation: 'Credit Suisse',
-  department: 'WTID 12',
-  role: 'Director',
+  department: 'Cross Boarder Information Management',
+  role: 'DIR',
   gender: 'Female',
-  location: 'Zurich',
-  about: Faker::Lorem.paragraph(2, false, 4),
-  looking_for: Faker::Lorem.sentence(2),
-  explanation: 'My Department does something very interesting, that is doing bla bla bla'
+  location: 'Uetlibergstrasse',
+  about: 'Start-Up idea, FinTech',
+  looking_for: 'Cognitive Computing, Golf, Chess Players',
+  explanation: 'My team is responsible that Relationship Managers know what they can and can not do in any country covered by the bank'
   )
 
 User.create!(
@@ -36,13 +35,13 @@ User.create!(
   email: 'fabi@duju.ch',
   password: 'dujurox',
   organisation: 'Credit Suisse',
-  department: 'WAAB',
-  role: 'Director',
+  department: 'Product Buffet',
+  role: 'DIR',
   gender: 'Male',
-  location: 'Zurich',
-  about: Faker::Lorem.paragraph(2, false, 4),
-  looking_for: Faker::Lorem.sentence(2),
-  explanation: 'My Department does something very interesting, that is doing bla bla bla'
+  location: 'Uetlibergstrasse',
+  about: 'Project Management Templates and Methods',
+  looking_for: 'Mentor',
+  explanation: 'My team is responsible that Relationship Managers have access to information about all products they are allowed to distribute in their area'
   )
 
 User.create!(
@@ -50,14 +49,14 @@ User.create!(
   last_name: 'Meier',
   email: 'josu@duju.ch',
   password: 'dujurox',
-  organisation: 'UBS',
-  department: 'WAAB',
-  role: 'Director',
+  organisation: 'Credit Suisse',
+  department: 'CRS and AEI implementation',
+  role: 'DIR',
   gender: 'Male',
-  location: 'Zurich',
-  about: Faker::Lorem.paragraph(2, false, 4),
-  looking_for: Faker::Lorem.sentence(2),
-  explanation: 'My Department does something very interesting, that is doing bla bla bla'
+  location: 'Uetlibergstrasse',
+  about: 'Sailing Events Zurichsee',
+  looking_for: 'Italian Tax Know How',
+  explanation: 'My team is responsible that the bank is ready to exchange information on Financial Accounts between international tax authorities '
   )
 
 User.create!(
@@ -65,14 +64,14 @@ User.create!(
   last_name: 'Meier',
   email: 'ri@duju.ch',
   password: 'dujurox',
-  organisation: 'UBS',
-  department: 'WAAB',
-  role: 'Director',
+  organisation: 'Credit Suisse',
+  department: 'IWM Solution Architecture',
+  role: 'DIR',
   gender: 'Female',
-  location: 'Zurich',
-  about: Faker::Lorem.paragraph(2, false, 4),
-  looking_for: Faker::Lorem.sentence(2),
-  explanation: 'My Department does something very interesting, that is doing bla bla bla'
+  location: 'Uetlibergstrasse',
+  about: 'Banking Platforms insights',
+  looking_for: 'Structured Products Know How',
+  explanation: 'My team is responsible that the bank builds consistent and strategically defined IT infrastructure'
   )
 
 puts 'Main users created'
@@ -87,12 +86,23 @@ puts 'Main users created'
     department: departments.sample,
     role: roles.sample,
     gender: genders.sample,
-    about: Faker::Lorem.paragraph(2, false, 4),
-    looking_for: Faker::Lorem.sentence(2),
+    about: 'US Tax know how',
+    looking_for: 'After work events',
     location: locations.sample,
-    explanation: 'My Department does something very interesting, that is doing bla bla bla'
+    explanation: 'My team is responsible that the bank builds consistent and strategically defined IT infrastructure'
   )
 end
 
 puts 'rest of users created'
+
+Network.create!(networker_one: User.first, networker_two: User.last)
+Network.create!(networker_one: User.second, networker_two: User.last)
+Network.create!(networker_one: User.third, networker_two: User.last)
+Network.create!(networker_one: User.fourth, networker_two: User.last)
+Network.create!(networker_one: User.first, networker_two: users.sample)
+Network.create!(networker_one: User.second, networker_two: users.sample)
+Network.create!(networker_one: User.third, networker_two: users.sample)
+Network.create!(networker_one: User.fourth, networker_two: users.sample)
+
+puts 'contacts created'
 
