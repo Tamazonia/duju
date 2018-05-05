9e7ba6d4c6a41813d746b5438e4c50410fa69cdc
Tamara Jost <facebookoftjo@gmail.com>
2018-05-05 12:06:32 +0100

    some changes

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,19 @@
-const { environment } = require('@rails/webpacker')
+// const { environment } = require('@rails/webpacker')
 
 // Bootstrap 3 has a dependency over jQuery:
+// const webpack = require('webpack')
+// environment.plugins.prepend('Provide',
+//   new webpack.ProvidePlugin({
+//     $: 'jquery',
+//     jQuery: 'jquery'
+//   })
+// )
+
+// module.exports = environment
+
+
+const { environment } = require('@rails/webpacker')
+
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
@@ -8,5 +21,4 @@ environment.plugins.prepend('Provide',
     jQuery: 'jquery'
   })
 )
-
 module.exports = environment
